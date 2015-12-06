5cb94e192d171413e4929caf386f3a17c667f5e8
David Rapson <david@davidrapson.co.uk>
2015-12-06 12:54:25 +0000

    Replace iTunes API with Spotify API
    HTTPs images and larger image sizes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import fs from 'fs';
-import http from 'http';
+import fetch from 'node-fetch';
 import express from 'express';
 
 const app = express();
@@ -10,31 +10,29 @@ app.set('port', (process.env.PORT || 8000));
 
 const random = arr => arr[Math.floor(Math.random() * arr.length)];
 
-const matchArtist = (albums, artist) => albums.find(x => x.artistName.includes(artist));
+function compositeData(album, spotifyData) {
+    return {
+        artist: album.artist,
+        title: album.title,
+        releaseDate: album.releaseDate,
+        score: parseInt(album.score, 10),
+        artwork: spotifyData && spotifyData.images[0].url || null,
+        itunesUrl: null,
+        spotify: spotifyData
+    };
+}
 
-const makeComposite = (album, itunesData) => ({
-    artist: album.artist,
-    title: album.title,
-    releaseDate: album.releaseDate,
-    score: parseInt(album.score, 10),
-    artwork: itunesData && itunesData.artworkUrl100 || null,
-    itunesUrl: itunesData && itunesData.collectionViewUrl || null
-});
-
-function getItunesData(album, cb) {
-    http.request({
-        host: 'itunes.apple.com',
-        path: `/search/?entity=album&term=${encodeURIComponent(album.title)}`
-    }, response => {
-        let str = '';
-        response.on('data', function (chunk) { str += chunk; });
-        response.on('end', function () {
-            let data = JSON.parse(str);
-            let itunesData = matchArtist(data.results, album.artist);
-            let composite =  makeComposite(album, itunesData)
-            cb && cb.call(this, composite);
-        });
-    }).end();
+function getSpotifyData(album) {
+    let endpoint = 'https://api.spotify.com/v1/search';
+    let title = encodeURIComponent(album.title);
+    let artist = encodeURIComponent(album.artist);
+    return fetch(`${endpoint}?q=artist:${artist}%20album:${title}&type=album`)
+        .then(resp => resp.json())
+        .then(data => data.albums.items)
+        .then(items => items.find(item => (
+            item.album_type === 'album' &&
+            item.available_markets.indexOf('GB') >= 0
+        )));
 }
 
 app.get('/album.json', (req, res) => {
@@ -43,9 +41,11 @@ app.get('/album.json', (req, res) => {
     fs.readFile('data/albums.json', 'utf8', (err,data) => {
         if (err) { return console.log(err); }
         let album = random((JSON.parse(data)).results);
-        getItunesData(album, composite => {
-            res.end(JSON.stringify(composite));
-        });
+        getSpotifyData(album)
+            .then(data => compositeData(album, data))
+            .then(result => {
+                res.end(JSON.stringify(result));
+            });
     });
 })
 
