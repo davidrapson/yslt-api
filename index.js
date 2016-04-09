914d96227a78189a94fcfa0a723fcb36eb9ed4a4
David Rapson <david@davidrapson.co.uk>
2016-04-09 14:07:26 +0100

    Switch to now for deployments
    - Use now
    - Remove babel
    - Upgrade deps

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 'use strict';
 
-require('babel-register')();
-require('./server.js');
+var fs = require('fs');
+var fetch = require('node-fetch');
+var express = require('express');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 9000));
+
+const random = arr => arr[Math.floor(Math.random() * arr.length)];
+
+function compositeData(album, spotifyData) {
+    return {
+        artist: album.artist,
+        title: album.title,
+        releaseDate: album.releaseDate,
+        score: parseInt(album.score, 10),
+        artwork: spotifyData && spotifyData.images[0].url || null,
+        itunesUrl: null,
+        spotify: spotifyData
+    };
+}
+
+function getSpotifyData(album) {
+    let endpoint = 'https://api.spotify.com/v1/search';
+    let title = encodeURIComponent(album.title);
+    let artist = encodeURIComponent(album.artist);
+    return fetch(`${endpoint}?q=artist:${artist}%20album:${title}&type=album`)
+        .then(resp => resp.json())
+        .then(data => data.albums.items)
+        .then(items => items.find(item => (
+            item.album_type === 'album' &&
+            item.available_markets.indexOf('GB') >= 0
+        )));
+}
+
+app.get('/album.json', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    fs.readFile('data/albums.json', 'utf8', (err,data) => {
+        if (err) { return console.log(err); }
+        let album = random((JSON.parse(data)).results);
+        getSpotifyData(album)
+            .then(data => compositeData(album, data))
+            .then(result => {
+                res.end(JSON.stringify(result));
+            });
+    });
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Node app is running at localhost: ${app.get('port')}`)
+});
